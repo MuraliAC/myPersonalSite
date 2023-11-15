b4fe85a6691c56114e5ee63370f3c986acc67806
Murali Krishna <94690714+MuraliAC@users.noreply.github.com>
2023-11-15 13:47:57 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function handleChange(currentIdentity, nextIdentity) {
+    document.getElementById(currentIdentity).style.display = "none"
+    document.getElementById(nextIdentity).style.display = "flex"
+}
